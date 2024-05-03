764b3ac5fbe1b1cb21c131ca6e6393ede4a5ff1f
Oliwier <oliwier.zagorski@yahoo.com>
2024-05-03 18:00:17 +0200

    added necessary fonts, created confirmation dialog and added some more functionality

--- src/pages/dashboard/Card.jsx
+++ src/pages/dashboard/Card.jsx
@@ -1,13 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import SearchIcon from '../../images/icons/search-icon.svg?react';
+import EditIcon from '../../images/icons/edit-icon.svg?react';
+import ReadyIcon from '../../images/icons/ready-icon.svg?react';
+import DeleteIcon from '../../images/icons/delete-icon.svg?react';
+import DeleteConfirmationDialog from './DeleteConfirmationDialog';
+import OwnerIcon from '../../images/icons/person-icon.svg?react';
+import CarIcon from '../../images/icons/car-icon.svg?react';
+import ClockIcon from '../../images/icons/clock-icon.svg?react';
 
 const Card = ({ model, owner, date }) => {
+  const navigate = useNavigate();
+  const [isDeleteDialogOpen, setDeleteDialogOpen] = useState(false);
+
+  const handleEditButton = () => {
+    navigate('/edit-details');
+  };
+
+  const handleDeleteButton = () => {
+    setDeleteDialogOpen(true);
+  };
+
   return (
     <div className="card">
       <ul className="car-data">
-        <li>Model: {model}</li>
-        <li>Owner: {owner}</li>
-        <li>Date: {date}</li>
+        <div className="card-icons">
+          <ReadyIcon className="icon pink-icon" />
+        </div>
+        <li>
+          <CarIcon className="icon pink-icon data-icon" />
+          {model}
+        </li>
+        <li>
+          <OwnerIcon className="icon pink-icon data-icon" />
+          {owner}
+        </li>
+        <li>
+          <ClockIcon className="icon pink-icon data-icon" />
+          {date}
+        </li>
+        <div className="card-btns card-icons">
+          <button
+            className="edit-btn icons-btn"
+            onClick={handleEditButton}
+          >
+            <EditIcon className="icon pink-icon" />
+          </button>
+          <button
+            className="trash-btn icons-btn"
+            onClick={handleDeleteButton}
+          >
+            <DeleteIcon className="icon pink-icon" />
+          </button>
+        </div>
       </ul>
+      <DeleteConfirmationDialog
+        isOpen={isDeleteDialogOpen}
+        onClose={() => setDeleteDialogOpen(false)}
+        onConfirm={() => {
+          setDeleteDialogOpen(false);
+        }}
+        onCancel={() => setDeleteDialogOpen(false)}
+      />
     </div>
   );
 };
