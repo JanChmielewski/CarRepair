aeb2ef8aef91af0cca22b434be8d89d0b10bee36
Maria <150148786+Mriaim@users.noreply.github.com>
2024-06-17 16:41:39 +0200

    Added Repository for car parts

---
+++ backend/src/main/java/pl/lodz/uni/wfis/mobilki/carrepair/repository/CarPartsRepo.java
@@ -0,0 +1,9 @@
+package pl.lodz.uni.wfis.mobilki.carrepair.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pl.lodz.uni.wfis.mobilki.carrepair.model.CarParts;
+
+@Repository
+public interface CarPartsRepo extends JpaRepository<CarParts, Long> {
+}
